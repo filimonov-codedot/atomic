3c5b5994f39cd69e9d5b8c721507539c05f34e20
filimonov-codedot <filimonov.codedot@gmail.com>
2021-03-03 12:16:18 +0300

    code formatting, bug fixes and edits

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -14,18 +14,34 @@ export const SEO = ({ titleTemplate, title, metaData }) => {
       meta={[
         {
           name: "description",
-          content: "Atomic",
+          content: metaTitle,
         },
         {
           name: "keywords",
           content: "atomic",
         },
         {
+          property: "og:url",
+          content: "https://atomic.vc"
+        },
+        {
           property: `og:title`,
           content: metaTitle,
         },
         {
+          property: 'og:description',
+          content: metaTitle,
+        },
+        {
           property: 'og:image',
+          content: metaData?.image.sizes.src
+        },
+        {
+          property: 'og:image:url',
+          content: metaData?.image.sizes.src
+        },
+        {
+          property: 'twitter:image',
           content: metaData?.image.sizes.src
         },
         {
